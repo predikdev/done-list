79372ece4356915a8708dc543190c276e811cbd2
predikdev <social.predik@gmail.com>
2025-03-19 18:20:48 +0100

    Basic project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
